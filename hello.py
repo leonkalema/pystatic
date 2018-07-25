ccff9481834104b75e6a087811b5028edc61cc3a
kaliboy <leon@taniza.se>
2018-07-25 17:39:55 +0200

    Scripts for Static code analysis

---
+++ hello.py
@@ -0,0 +1,34 @@
+print("Please Nigeria score")
+if 5 > 1:
+	print("five is greater than One")
+if 0 == 0 :
+	print("Its a draw")
+	
+if 34 < 1984:
+	("You were born in 1984") 
+#we also can comment in python 
+"""And also this 
+Enachio picasio guy 
+is very annoying """
+
+
+ #variable time :) 
+nigeria = 0
+argentina = 1
+
+print(nigeria)
+print(argentina)
+
+x = 4
+x = "Serugo" 
+print (x)
+print ("vs" + x)
+
+s = "the refs are cheats "
+d = "rascism is real"
+
+z = s + d
+
+print(z)
+
+
